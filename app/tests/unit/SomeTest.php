818cb18877e3212c1a2e9f32ee1cb2159a297a8e
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-19 13:22:59 +0300

    Init codeception

---
+++ app/tests/unit/SomeTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DmitriiKoziuk\travisStudy\tests\unit;
+
+use DmitriiKoziuk\travisStudy\tests;
+
+class SomeTest extends \Codeception\Test\Unit
+{
+    protected tests\UnitTester $tester;
+
+
+    // tests
+    public function testSomeFeature()
+    {
+        $this->assertTrue(true);
+    }
+}
